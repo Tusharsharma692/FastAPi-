1cc2692683306f9e214d2cfec3308bbbd9b0380e
Ashish95sharma <ashish.2024cse1190@kiet.edu>
2026-06-06 20:44:27 +0530

    push

--- main.py
+++ main.py
@@ -3,7 +3,26 @@ from fastapi import FastAPI
 app= FastAPI()
 
 
+# home route
 @app.get("/")
 def home():
     return {"message": "Hello World"}
+
+
+# about route
+
+@app.get("/about")
+def about():
+    return {"message": "This is about page"}
+
+# different return type
+@app.get("/users")
+def get_users():
+    return {
+        "users": [
+            {"name": "John", "age": 30},
+            {"name": "Jane", "age": 25},
+            {"name": "Doe", "age": 35}
+        ]
+    }
 
